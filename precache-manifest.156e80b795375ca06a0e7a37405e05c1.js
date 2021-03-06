a75979744a9d3ad563432303646e86798ca0d0de
gh28942 <GerH@example.com>
2021-03-06 18:05:09 +0100

    Updates

---
+++ precache-manifest.156e80b795375ca06a0e7a37405e05c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447c003adf426c8957f2457921fcab18",
+    "url": "/eurojackpot-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/eurojackpot-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "b438d4abdfd257311c1e",
+    "url": "/eurojackpot-simulator/static/css/main.0f754ba6.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/eurojackpot-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eurojackpot-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b438d4abdfd257311c1e",
+    "url": "/eurojackpot-simulator/static/js/main.a5751e58.chunk.js"
+  },
+  {
+    "revision": "4ea6bd41f71570c36007",
+    "url": "/eurojackpot-simulator/static/js/runtime-main.601c31d0.js"
+  },
+  {
+    "revision": "14f57f6562ef5d253e0b53c26bd4b738",
+    "url": "/eurojackpot-simulator/static/media/stars.14f57f65.jpg"
+  }
+]);
